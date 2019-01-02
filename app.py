f993df16683ecfc79d366305c9a9f191b7086927
Siddharth <sidjain1412@gmail.com>
2019-01-02 22:44:36 +0530

    Added basic route handling for login and main page

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/to_do')
+def to_do():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
